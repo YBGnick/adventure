79e5f2db0565de8a65693bacf462579c36aa367e
YBGnick <reyioa2024@gmail.com>
2025-12-19 13:29:43 +0000

    day1

---
+++ src/Adventure.java
@@ -0,0 +1,55 @@
+
+// Enter your Hero's name (user input)
+// You have fallen from a tree and cannot remember anything about yourself. It is up to you, (Hero name), to rediscover your past and travel home.
+// you begin with 100 Hitpoints
+// CROSSROADS 1: You come accross 2 paths with signs that describe the paths, they are called the North and South paths. type your desired path
+// You chose the North path! The nor
+h path leads you to your hometown with few inhabitants, yet there is a town in between that may be hostile. 
+// you chose the South path! The south path takes you through the vast grasslands. Here, you can travel directly home but there may be dangers in your way.
+// Along the North path, you encounter a small dog that looks hungry
+// DECISION: do you want to take this dog as your friend? (Y or N)   
+// IF takes dog, the dog bites you and you are dead. 
+// You lose! type YES to play again
+// IF you don't take the dog, you walk to town past the dog anyway without it
+// Once you reach the small town, it appears like it is a very nice place. you may decide to cointnue or stay behind
+// IF user stays behind: You lost! Your one goall was to travel home and you got caught up in the small town
+// You lose! type YES to play again!
+// IF user goes to hometown: You walk back home, hoping to rediscover who you are
+// At home: "Hero name!" Shouted an older woman, suddenly, all of your memories flood back
+// You Win! Type yes to play again
+// IF takes south path: You walk accross the path, when a mountain is in the way, you can either scale the mountain or go around
+// IF user selects scale the mountain: While scaling the mountain, you encounter a friendly villager that knows the way to your hometown
+// The villager takes you to hometown, (Go to line 16)
+// IF you chose to go around the mountain: Unfortunately the trail takes you around the mountain into the woods. You get lost
+// You lost! type YES to try again!
+
+public class Adventure {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner s = new Scanner(System.in);
+        String hero = s.nextLine();
+        int gold = 100;
+        int health = 100;
+        
+
+        System.out.println("You have fallen from a tree and cannot remember anything about yourself. It is up to you," + hero + ", to rediscover your past")
+
+        String direction = "";
+HP        while (direction != "left" || direction != "right") {
+            System.out.println("CROSSROADS 1: You come accross 2 paths with signs that describe the paths, they are called the North and South paths. type your desired path");
+            direction = s.nextLine();
+        }
+        
+        if (direction == "north") {
+
+        
+        
+        }
+        
+    }
+
+
+}
