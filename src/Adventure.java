f4dd1b7efa822f3a1764b702e193e846ee69e162
YBGnick <reyioa2024@gmail.com>
2025-12-22 14:31:07 +0000

    done

--- src/Adventure.java
+++ src/Adventure.java
@@ -3,8 +3,7 @@
 // You have fallen from a tree and cannot remember anything about yourself. It is up to you, (Hero name), to rediscover your past and travel home.
 // you begin with 100 Hitpoints
 // CROSSROADS 1: You come accross 2 paths with signs that describe the paths, they are called the North and South paths. type your desired path
-// You chose the North path! The nor
-h path leads you to your hometown with few inhabitants, yet there is a town in between that may be hostile. 
+// You chose the North path! The norh path leads you to your hometown with few inhabitants, yet there is a town in between that may be hostile. 
 // you chose the South path! The south path takes you through the vast grasslands. Here, you can travel directly home but there may be dangers in your way.
 // Along the North path, you encounter a small dog that looks hungry
 // DECISION: do you want to take this dog as your friend? (Y or N)   
@@ -23,33 +22,122 @@ h path leads you to your hometown with few inhabitants, yet there is a town in b
 // IF you chose to go around the mountain: Unfortunately the trail takes you around the mountain into the woods. You get lost
 // You lost! type YES to try again!
 
+import java.util.Scanner;
+
 public class Adventure {
 
-
-    public static void main(String[] args) {
-
-
+    static void adventure() {
         Scanner s = new Scanner(System.in);
         String hero = s.nextLine();
-        int gold = 100;
-        int health = 100;
-        
-
-        System.out.println("You have fallen from a tree and cannot remember anything about yourself. It is up to you," + hero + ", to rediscover your past")
+        System.out.println("You have fallen from a tree and cannot remember anything about yourself. It is up to you, " + hero + ", to rediscover your past");
 
         String direction = "";
-HP        while (direction != "left" || direction != "right") {
+        System.out.println("CROSSROADS 1: You come accross 2 paths with signs that describe the paths, they are called the North and South paths. type your desired path");
+        direction = s.nextLine();
+        while (!(direction == "N" || direction == "S")) {
+            System.out.println("Redo");
             System.out.println("CROSSROADS 1: You come accross 2 paths with signs that describe the paths, they are called the North and South paths. type your desired path");
             direction = s.nextLine();
         }
-        
-        if (direction == "north") {
+        System.out.println(direction);
+        if (direction == "N") {
+            System.out.println("You chose the North path! The norh path leads you to your hometown with few inhabitants, yet there is a town in between that may be hostile.");
+            System.out.println("Along the North path, you encounter a small dog that looks hungry");
+            System.out.println("do you want to take this dog as your friend? (Y or N)");
+            direction = s.nextLine();
+            while (direction != "N" || direction != "Y") {
+                System.out.println("Redo");
+                System.out.println("do you want to take this dog as your friend? (Y or N)");
+                direction = s.nextLine();
+            }
 
-        
-        
+            if (direction == "Y") {
+                System.out.println("the dog bites you and you are dead.");
+                System.out.println("You lose! type YES to play again");
+                s.close();
+                return;
+            } 
+            System.out.println("you walk to town past the dog anyway without it");
+            System.out.println("Once you reach the small town, it appears like it is a very nice place. you may decide to cointnue or stay behind");
+
+            System.out.println("Do you stay behind? (Y or N)");
+            direction = s.nextLine();
+            while (direction != "N" || direction != "Y") {
+                System.out.println("Redo");
+                System.out.println("Do you stay behind? (Y or N)");
+                direction = s.nextLine();
+            }
+            if (direction == "N") {
+                System.out.println("You lost! Your one goall was to travel home and you got caught up in the small town");
+                System.out.println("You lose! type YES to play again!");
+                s.close();
+                return;
+            } 
+
+            System.out.println("You walk back home, hoping to rediscover who you are");
+            System.out.println(hero + "! Shouted an older woman, suddenly, all of your memories flood back");
+            System.out.println("You Win! Type yes to play again");
+
+            
         }
-        
+        if (direction == "S") {
+            System.out.println("You walk accross the path, when a mountain is in the way, you can either scale the mountain or go around");
+            System.out.println("Go up the M or W around");
+            while (direction != "M" || direction != "W") {
+                System.out.println("Redo");
+                System.out.println("Go up the M or W around");
+                direction = s.nextLine();
+            } 
+            if (direction == "M") {
+                System.out.println("While scaling the mountain, you encounter a friendly villager that knows the way to your hometown");
+                System.out.println("you encounter a small dog that looks hungry");
+                System.out.println("do you want to take this dog as your friend? (Y or N)");
+                direction = s.nextLine();
+                while (direction != "N" || direction != "Y") {
+                    System.out.println("Redo");
+                    System.out.println("do you want to take this dog as your friend? (Y or N)");
+                    direction = s.nextLine();
+                }
+
+                if (direction == "Y") {
+                    System.out.println("the dog bites you and you are dead.");
+                    System.out.println("You lose! type YES to play again");
+                s.close();
+                    return;
+                } 
+                System.out.println("you walk to town past the dog anyway without it");
+                System.out.println("Once you reach the small town, it appears like it is a very nice place. you may decide to cointnue or stay behind");
+
+                System.out.println("Do you stay behind? (Y or N)");
+                direction = s.nextLine();
+                while (direction != "N" || direction != "Y") {
+                    System.out.println("Redo");
+                    System.out.println("Do you stay behind? (Y or N)");
+                    direction = s.nextLine();
+                }
+                if (direction == "N") {
+                    System.out.println("You lost! Your one goall was to travel home and you got caught up in the small town");
+                    System.out.println("You lose! type YES to play again!");
+                s.close();
+                    return;
+                } 
+
+                System.out.println("You walk back home, hoping to rediscover who you are");
+                System.out.println(hero + "! Shouted an older woman, suddenly, all of your memories flood back");
+                System.out.println("You Win! Type yes to play again");
+            } else {
+                System.out.println("Unfortunately the trail takes you around the mountain into the woods. You get lost");
+                System.out.println("You lost! type YES to try again!");
+            }
+        }
+        s.close();
+        }
+
+    public static void main(String[] args) {
+    
+        Scanner s = new Scanner(System.in);
+            adventure();
     }
 
-
 }
+
